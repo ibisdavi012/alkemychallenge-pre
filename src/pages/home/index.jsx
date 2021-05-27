97549968daa364d71233fdd785047be20458a1e0
David Márquez <ibisdavi012@gmail.com>
2021-05-27 16:40:45 +0200

    First Stylecomponent

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,16 @@
 import styles from './home.module.css';
 import React from 'react';
 import PageContent from '../../components/PageContent';
+import styled from 'styled-components';
 
 export default class Home extends React.Component {
+       
     render(){
+        const PageTitle = styled.h2`
+        `;
         return (
             <PageContent>
-                <h2>Home</h2>
+                <PageTitle>Home</PageTitle>
                 <p className={styles.blueParagraph}>Paragraph from Home.js</p>
             </PageContent>
         );
