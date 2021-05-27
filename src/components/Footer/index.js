96604bf6a82cb3bdeca0d599fd3cb8c650cd053c
David Márquez <ibisdavi012@gmail.com>
2021-05-27 20:20:09 +0200

    New Commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,20 @@
 import styles from './Footer.module.css';
 import CornerLeft from './assets/corner_left.svg'; 
 import CornerRight from './assets/corner_right.svg';
-import Flare from './assets/flare.svg';
 import React from 'react';
 
 class Footer extends React.Component {
     render(){        
         return (
-            <header className={`container ${styles.header}`}>
-                    <img src={CornerLeft} alt="" />
+            <footer className={`container ${styles.footer}`}>
+                    <img className={styles.cornerImage} src={CornerLeft} alt="" />
                     
                     <div className={styles.flareContainer}>
-                        <img src={Flare} alt="" className={styles.flare}/>
+                        <img alt="" className={styles.flare}/>
                     </div>
 
-                    <img src={CornerRight} alt=""/>
-            </header>
+                    <img className={styles.cornerImage} src={CornerRight} alt=""/>
+            </footer>
         )
     }
 }
--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -3,9 +3,13 @@ import React, { Component } from 'react'
 export default class LoginForm extends Component {
   render() {
     return (
-      <div>
-        USER & PASS
-      </div>
+      <form className="login-fom">
+          <label htmlFor="">Email</label>
+          <input type="email" name="" id="" />
+           <label htmlFor="">Password</label>
+          <input type="password" name="" id="" />
+          <button>Iniciar Sesión</button>
+      </form>
     )
   }
 }
