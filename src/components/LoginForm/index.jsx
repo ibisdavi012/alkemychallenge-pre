d0b54591b3729e2e1372c7b1f928d9499f6b2d0d
David Márquez <ibisdavi012@gmail.com>
2021-05-28 09:35:22 +0200

    New Commit

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -6,20 +6,34 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import IronManImage from './assets/ironman.png';
 import axios from 'axios';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 export default class LoginForm extends Component {
   render() {
     const validateLogin = (e) => {
       e.preventDefault(); 
-      axios.get('http://challenge-react.alkemy.org/',{params:{
-        email:'challenge@alkemy.org', password:'react'
-      }})
-      .then(data=>{
-console.log(data)
-      })
+      /*axios.post(http://challenge-react.alkemy.org/',{email:'challenge@alkemy.org', password:'react'})
+      .then(data>{console.log(data)})
       .catch()
-      .then()
+      .then(()=>{})*/
     }
+      const initialValues = {email:'', password:''};
+
+      const onSubmit = (values, setSubmitting) => {
+        console.log('Submitting',values);
+      }
+
+      const validationSchema = values => {
+        const schema = Yup.object({
+          email: Yup.string().email('La dirección de correo no tiene un formato válido.').require('La dirección de correo es obligatoria.'),
+          password: Yup.string().required('La contraseña es requerida para poder validar su acceso.')
+        });
+
+        console.log('Validating',values);
+
+        return schema;
+      }
 
     return (
 
@@ -37,24 +51,26 @@ console.log(data)
         </Row>
 
         <Row className="justify-content-center align-middle">
-          <Col lg={5} xs={8}>
-            <Form onSubmit={validateLogin}>
+          <Col lg={5} xs={8}>            
+         <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} >
+           {(formik)=>(<Form onSubmit={formik.handleSubmit}>
               <Form.Group controlId="userEmail">
                 <Form.Label>Correo Electrónico</Form.Label>
-                <Form.Control type="email" placeholder="Correo Electrónico" />
+                <Form.Control type="email" placeholder="Correo Electrónico" name="email" {...formik.getFieldProps('email')}/>
+                {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
               </Form.Group>
         
               <Form.Group controlId="userPassword">
                 <Form.Label>Contraseña</Form.Label>
-                <Form.Control type="password" placeholder="Contraseña" />
+                <Form.Control type="password" placeholder="Contraseña" name="password" {...formik.getFieldProps('password')}/>
               </Form.Group>
               <Form.Group controlId="userPassword">
                 <Button variant="primary" type="submit" >
                   Iniciar Sesión
                 </Button>
               </Form.Group>
-            </Form>
-         
+            </Form>)}
+         </Formik>
           </Col>
         </Row>
         </div>
